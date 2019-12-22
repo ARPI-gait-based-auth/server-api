8a9b854c5ce609edd2529741403103ea9f2c30ff
Tilen Tomakić <tilen@tomakic.com>
2019-12-22 18:53:14 +0100

    Port change fix.

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 require('./route-explore')({express, app});
 require('./route-record')(app);
 
-app.listen((+ process.env['PORT']) || 3900);
-console.log('Server started on port ' + (+ process.env['PORT']) || 3900);
+let port = (+ process.env['PORT']) || 3900;
+app.listen(port);
+console.log('Server started on port ' + port);
